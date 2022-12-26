6d6d8c08a55080f400c35662be283c1da1f88c71
Hellx2 <95957618+Hellx2@users.noreply.github.com>
2022-12-26 21:04:25 +1100

    Create math.hpp

---
+++ math.hpp
@@ -0,0 +1,11 @@
+long double min(long double x, long double y) {
+  return x < y ? x : y;
+}
+
+long double max(long double x, long double y) {
+  return x > y ? x : y;
+}
+
+long double abs(long double x) {
+  return x < 0 ? -x : x;
+}
