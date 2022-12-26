bfa98631a9adc14313f81f7f57994f6592b65a86
Hellx2 <95957618+Hellx2@users.noreply.github.com>
2022-12-27 09:52:26 +1100

    Update util.js

--- util.js
+++ util.js
@@ -9,3 +9,20 @@ export let range = (start, stop = undefined, step = undefined) {
   for(let i = start; i < stop; i += step) r.push(i);
   return r;
 }
+
+let math = {
+  max: (...nums) => {
+    let max = -Infinity;
+    for(let num of nums)
+      if(Number(num) > max) 
+        max = Number(num);
+    return max;
+  },
+  min: (...nums) => {
+    let min = Infinity;
+    for(let num of nums)
+      if(Number(num) < min) 
+        min = Number(num);
+    return min;
+  }
+}
