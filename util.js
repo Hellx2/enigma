f38953e09d0ac29200999bebc764046ebcd0d6bb
Hellx2 <95957618+Hellx2@users.noreply.github.com>
2022-12-27 09:48:44 +1100

    Update util.js

--- util.js
+++ util.js
@@ -4,4 +4,8 @@ export let range = (start, stop = undefined, step = undefined) {
     start = 0;
   }
   if(!step) step = 1;
+  if(start > stop || step <= 0 || (start - stop) % step != 0) throw new RangeError();
+  let r = [];
+  for(let i = start; i < stop; i += step) r.push(i);
+  return r;
 }
