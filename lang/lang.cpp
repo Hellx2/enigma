637f1e8d1298824e112983522648c72e9fef3566
Hellx2 <95957618+Hellx2@users.noreply.github.com>
2022-12-26 19:35:36 +1100

    Update lang.cpp

--- lang/lang.cpp
+++ lang/lang.cpp
@@ -1,20 +1,43 @@
 #include <fstream>
 #include <iostream>
 #include <cstdio>
+#include <cassert>
+#include <string.h>
+#include <cstdlib>
+
 #include "lang.hpp"
+
+typedef std::string str;
 
 using std::printf;
 using std::scanf;
 
 int main(int argc, char **argv) {
+  system("@ECHO OFF");
   char *src = "";
+  bool isFile = false;
   if(argc > 0) {
     for(int i = 0; i < argc - 1; i++)
       src += argv[i] + " ";
     src += argv[argc - 1];
   } else {
-    printf("Path to file: ");
-    scanf("%s", &src);
+    char t = tolower(input<char>("Read from file? (y/n)"));
+    assert(t == "y" || t == "n");
+    isFile = (t == "y");
+    if(isFile) src = input("Path to file: ");
   }
+  char *t1;
+  if(isFile) {
+    ifstream f(src);
+    while(getline(f, t1)) read(t1);
+  } else while((t1 = input(":: >>> ")) != "end" && t1 != 0 && t1 != "") read(t1);
+  std::cout << "Terminating program...\n"
+            << "Program terminated.";
+  system("PAUSE");
   return 0;
+}
+
+void read(char *line) {
+  str ln(line);
+  // ...
 }
